a16095e5d6d93563993cd3aafcaea372cad0bf2a
chimegerdene <chimgeebatuh@gmail.com>
2025-04-08 11:35:54 +0800

    user filter

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express, { json } from "express";
-
+import { v4 as uuidv4 } from "uuid";
 const port = 8080;
 
 const app = express();
@@ -12,18 +12,29 @@ app.use(json());
 const users = [];
 
 app.post("/user/create", (req, res) => {
-  const { username, gender, age, email } = req.body;
+  const { username, gender, age, email, id } = req.body;
   users.push({
     username,
     gender,
     age,
     email,
+    id: uuidv4(),
   });
-  console.log(users, "users");
   res.send({
     success: true,
     message: "done",
   });
+});
+
+app.get("/users", (_, res) => {
+  res.send(users);
+});
+
+app.get("/user", (req, res) => {
+  // const search = "test@gmail.com";
+  const { id } = req.body;
+  const filter = users.find((user) => user.id === id);
+  res.send(filter);
 });
 
 app.listen(port, () => {
